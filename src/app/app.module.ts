34a8a47274a05264e74fb3ead76d0c17315ea780
paulo henrique <paulo_henrique-16@live.com>
2018-12-14 16:20:11 -0200

    criação das telas de introdução

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { LoginPageModule } from './../pages/login/login.module';
+import { IntroPageModule } from '../pages/intro/intro.module';
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +15,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    LoginPageModule,
+    IntroPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
